4945bae73d84cc11b7e5c6360b2a50f8a5e05031
Carlos Ch <carlos.echc11@gmail.com>
2023-04-29 14:43:54 -0600

    md

--- extras.md
+++ extras.md
@@ -4,3 +4,4 @@ https://www.youtube.com/watch?v=gTwXG8lC2GM
 
 Redis:
 
+https://device.sso.us-east-1.amazonaws.com/https://device.sso.us-east-1.amazonaws.com/
--- src/middlewares/basicErrorHandlers.js
+++ src/middlewares/basicErrorHandlers.js
@@ -1,8 +1,8 @@
 export const errorHandlers = (err, req, res, next) => {
-  console.log("err.message :>> ", err.message);
-  console.log("err.code :>> ", err.code);
+  console.error("err.code :>> ", err.code);
+  console.error("err.message :>> ", err.message);
   // send response
   return res
-    .status(500)
-    .send({ errorCode: err.code, errorMessage: err.message });
+    .status(err.status || 500)
+    .send({ errorCode: err.code, errorMessage: err.userMessage });
 };
